78e7369f9cc8853ef177bf47c67b61d13e3c347b
Arpan Swain <kankalase07@gmail.com>
2023-10-04 12:34:09 +0530

    Admin

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -6,6 +6,9 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = {'meals':["pizza","pasta"]}
+        return context
 class MenuItemDetail(generic.DetailView):
     model = Item
     template_name = "menu_item_detail.html"
